3ee6bbcff03a4054e88fe1347769f96a52a0d0f6
Yusuf Khan <kkyusufk@gmail.com>
2025-11-04 19:30:29 +0530

    fixing deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// Import debug utility in development
-if (import.meta.env.DEV) {
-  import('./utils/debugAuth')
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
